101c6d03ecb86f0c790b379ebadf182d74d0fb8b
voa2000 <voa2000@hotmail.com>
2019-08-24 19:19:16 +0100

    Argv modules

--- app.js
+++ app.js
@@ -1,7 +1,68 @@
 const validator = require("validator");
 const chalk = require("chalk");
 const getNotes = require("./notes");
+const yargs = require("yargs");
 
-console.log(getNotes());
-console.log(chalk.green("viv@com"));
-console.log(chalk.blue.bgRed.bold("Vivienne"));
+// Customize yargs version
+yargs.version("1.1.0");
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: function(argv) {
+    console.log("Title: ", argv.title);
+    console.log("Body: ", argv.body);
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing the note");
+  }
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: function() {
+    console.log("Listing out all notes");
+  }
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function() {
+    console.log("Reading a note");
+  }
+});
+
+console.log(yargs.argv);
+
+// if (command === "add") {
+//     console.log("Adding operation selected");
+//     if (argv.title>0){console.log(object);}
+// } else if (command === "sub") {
+//   console.log("Subtracting operation selected");
+// } else {
+//   console.log("Invalid command");
+// }
+// console.log(title);
