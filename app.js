704d2b25506e1dc1da96d91b2ac44e7890a628fc
voa2000 <voa2000@hotmail.com>
2019-08-24 15:34:24 +0100

    Added Validator and chalk packages

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes");
 
 console.log(getNotes());
+console.log(chalk.green("viv@com"));
+console.log(chalk.blue.bgRed.bold("Vivienne"));
